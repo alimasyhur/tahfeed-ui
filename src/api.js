f7146b8ca0425ead8c34914b104da1e7b856d7ea
Ali Masyhur <jegrag4ever@gmail.com>
2025-02-12 20:01:50 +0700

    feat(baseurl): try baseurl dev

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiService = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'http://dev-api.tahfeed.com/api/',
   withCredentials: true
 })
 
